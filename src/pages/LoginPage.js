4047a5bd9a0bf07713d6b1b07cb8bbc1170fd146
Vincent Bas <vbas@edu.ece.fr>
2018-01-03 22:34:23 +0100

    resolve Cors

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 class LoginPage extends Component {
   login = () => {
-    axios.post('/URL', {
-      username: document.getElementById('email'),
-      password: document.getElementById('password')
+    axios.post('/api/authenticate', {
+      username:'jeff',
+      password:'jeff'
     })
       .then(function (response) {
         console.log(response);
@@ -17,72 +17,72 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div class="container">
-        <form class="form-horizontal" role="form" method="POST" action="/login">
-          <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
+      <div className="container">
+        <form className="form-horizontal" role="form" method="POST" action="/login">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6">
               <h2>Please Login</h2>
               <hr/>
               </div>
             </div>
-            <div class="row">
-              <div class="col-md-3"></div>
-              <div class="col-md-6">
-                <div class="form-group has-danger">
-                  <label class="sr-only" for="email">E-Mail Address</label>
-                  <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-                    <div class="input-group-addon"><i class="fa fa-at"></i></div>
-                    <input type="text" name="email" class="form-control" id="email"
-                      placeholder="you@example.com" required autofocus />
+            <div className="row">
+              <div className="col-md-3"></div>
+              <div className="col-md-6">
+                <div className="form-group has-danger">
+                  <label className="sr-only" htmlFor="username">E-Mail Address</label>
+                  <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                    <div className="input-group-addon"><i className="fa fa-at"></i></div>
+                    <input type="text" name="username" className="form-control" id="username"
+                      placeholder="you@example.com" />
                       </div>
                   </div>
                 </div>
-                <div class="col-md-3">
-                  <div class="form-control-feedback">
-                    <span class="text-danger align-middle">
-                      <i class="fa fa-close"></i> Example error message
+                <div className="col-md-3">
+                  <div className="form-control-feedback">
+                    <span className="text-danger align-middle">
+                      <i className="fa fa-close"></i> Example error message
                       </span>
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <label class="sr-only" for="password">Password</label>
-                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-                      <div class="input-group-addon"><i class="fa fa-key"></i></div>
-                      <input type="password" name="password" class="form-control" id="password"
-                        placeholder="Password" required />
+              <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label className="sr-only" htmlFor="password">Password</label>
+                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                      <div className="input-group-addon"><i className="fa fa-key"></i></div>
+                      <input type="password" name="password" className="form-control" id="password"
+                        placeholder="Password"/>
                       </div>
                     </div>
                   </div>
-                  <div class="col-md-3">
-                    <div class="form-control-feedback">
-                      <span class="text-danger align-middle">
+                  <div className="col-md-3">
+                    <div className="form-control-feedback">
+                      <span className="text-danger align-middle">
                         Put password error message here
                       </span>
                     </div>
                   </div>
                 </div>
-                <div class="row">
-                  <div class="col-md-3"></div>
-                  <div class="col-md-6">
-                    <div class="form-check mb-2 mr-sm-2 mb-sm-0">
-                      <label class="form-check-label">
-                        <input class="form-check-input" name="remember"
+                <div className="row">
+                  <div className="col-md-3"></div>
+                  <div className="col-md-6">
+                    <div className="form-check mb-2 mr-sm-2 mb-sm-0">
+                      <label className="form-check-label">
+                        <input className="form-check-input" name="remember"
                           type="checkbox" />
                           <span>Remember me</span>
                       </label>
                   </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                      <button type="submit" class="btn btn-success" onClick={this.login()}><i class="fa fa-sign-in"></i> Login</button>
-                      <a class="btn btn-link" href="/password/reset">Forgot Your Password?</a>
+                  <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                      <button type="submit" className="btn btn-success" onClick={()=>this.login()}><i className="fa fa-sign-in"></i> Login</button>
+                      <a className="btn btn-link" href="/password/reset">Forgot Your Password?</a>
                     </div>
                   </div>
                 </form>
